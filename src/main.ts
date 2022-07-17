a1873989531da204070d92d835d7b6109c8855d6
ratomsky <max.work.dev@gmail.com>
2022-07-17 23:26:07 +0300

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { resolve } from 'path';
+import { cwd } from 'process';
+import { parse } from 'yaml';
 import { AppModule } from './app.module';
 
 const PORT = process.env.PORT ?? 4000;
@@ -7,6 +12,13 @@ const PORT = process.env.PORT ?? 4000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const document = await readFile(resolve(cwd(), 'doc', 'api.yaml'), {
+    encoding: 'utf8',
+  });
+
+  SwaggerModule.setup('doc', app, parse(document));
+
   await app.listen(PORT);
 }
 bootstrap();
